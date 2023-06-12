701f9fa55e85b8eb9855471ff7c540aa6ab03572
oaydas23 <oaydas@resolutebi.com>
2023-06-12 12:15:39 -0400

    Added login and register feature

--- view.py
+++ view.py
@@ -1,8 +1,19 @@
 from github import Github
 import os
 import sqlite3
-from flask import Flask, render_template, request
 from datetime import datetime
+import random
+from flask import Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import string
+import requests
+import urllib.parse
+from random import sample
+from flask_session import Session
+
+from functools import wraps
 
 
 g = Github("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")
@@ -10,14 +21,131 @@ org = g.get_organization("MadDogTechnology")
 
 app = Flask(__name__)
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/", methods=["GET"])
+@login_required
 def index():
     return render_template("home.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+
+
+    session.clear()
+    table = sqlite3.connect("data/database.db")
+    cursor = table.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(id))")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html")
+
+        # Query database for username
+        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        rows = cursor.fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
+            return render_template("login.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    session.clear()
+    table = sqlite3.connect("data/database.db")
+    cursor = table.cursor()
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("register.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("register.html")
+
+        elif not request.form.get("confirmation"):
+            return render_template("register.html")
+
+        elif request.form.get("confirmation") != request.form.get("password"):
+            return render_template("register.html")
+
+        # Query database for username
+        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        rows = cursor.fetchall()
+
+        if len(rows) == 1:
+            return render_template("register.html")
+
+        # Insert username and password
+        cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (request.form.get(
+            "username"), generate_password_hash(request.form.get("password"))))
+
+        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        rows = cursor.fetchall()
+        session["user_id"] = rows[0][0]
+        # Redirect user to login
+        table.commit()
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+    
 
 @app.route("/artifact", methods=["GET", "POST"])
 def artifact():
+
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
 
@@ -67,6 +195,7 @@ def artifact():
 
 @app.route("/commit", methods=["POST", "GET"])
 def commit():
+
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
 
