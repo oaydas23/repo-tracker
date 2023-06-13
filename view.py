5e47b968b00d3d3b2fda03e2984d713d8c7e956a
oaydas23 <oaydas@resolutebi.com>
2023-06-13 14:35:27 -0400

    Connect view.py to PostgreSQL

--- view.py
+++ view.py
@@ -7,59 +7,136 @@ from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 import string
 import requests
 import urllib.parse
 from random import sample
-from flask_session import Session
 import re
-
 from functools import wraps
 
-
+# Github Token
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 g = Github("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")
 org = g.get_organization("MadDogTechnology")
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 
 app = Flask(__name__)
 
+# PostgreSQL Database
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:password@localhost:5000/repo"
+db = SQLAlchemy(app)
+
+
+# Commit Table
+class Commit(db.Model):
+    __tablename__ = "commits"
+    id = db.Column(db.Integer, primary_key=True)
+    repo = db.Column(db.String)
+    message = db.Column(db.String)
+    author = db.Column(db.String)
+    date = db.Column(db.String)
+    archive = db.Column(db.String)
+
+    def __init__(self, repo, message, author, date, archive):
+        self.repo = repo
+        self.message = message
+        self.author = author
+        self.date = date
+        self.archive = archive
+
+
+# Gradle Table
+class Gradle(db.Model):
+    __tablename__ = "gradle"
+    id = db.Column(db.Integer, primary_key=True)
+    pname = db.Column(db.String)
+    gname = db.Column(db.String)
+    bnum = db.Column(db.String)
+    bdate = db.Column(db.String)
+    artifacts = db.Column(db.String)
+
+    def __init__(self, pname, gname, bnum, bdate, artifacts):
+        self.pname = pname
+        self.gname = gname
+        self.bnum = bnum
+        self.bdate = bdate
+        self.artifacts = artifacts
+
+
+# Users Table
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    hash = db.Column(db.String)
+
+    def __init__(self, username, hash):
+        self.username = username
+        self.hash = hash
+
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Session
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+# Login
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 def login_required(f):
     """
     Decorate routes to require login.
 
     https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
+
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
+
     return decorated_function
+
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 @app.route("/", methods=["GET"])
 @login_required
 def index():
+    db.create_all()
+
     return render_template("home.html")
 
+
+# Login, Logout & Register
+# /////////////////////////////////////////////////////////////////////////////////////////////////
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
     # Forget any user_id
 
-
     session.clear()
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(id))")
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER, username TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(id))"
+    )
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return render_template("login.html")
@@ -69,11 +146,15 @@ def login():
             return render_template("login.html")
 
         # Query database for username
-        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (request.form.get("username"),)
+        )
         rows = cursor.fetchall()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0][2], request.form.get("password")
+        ):
             return render_template("login.html")
 
         # Remember which user has logged in
@@ -100,6 +181,7 @@ def logout():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
     session.clear()
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
@@ -108,47 +190,63 @@ def register():
         # Ensure username was submitted
         if not request.form.get("username"):
             return render_template("register.html")
-
+        
         # Ensure password was submitted
         elif not request.form.get("password"):
             return render_template("register.html")
-
         password = request.form.get("password")
 
         # Ensure password meets the requirements
-        if len(password) < 8 or not re.search(r"[A-Z]", password) or not re.search(r"[!@#$%^&*()_+\-=[\]{};':\"\\|,.<>/?]", password):
+        if (
+            len(password) < 8
+            or not re.search(r"[A-Z]", password)
+            or not re.search(r"[!@#$%^&*()_+\-=[\]{};':\"\\|,.<>/?]", password)
+        ):
             return render_template("register.html")
-
+        
         # Ensure confirmation matches the password
         elif request.form.get("confirmation") != password:
             return render_template("register.html")
-
+        
         # Query database for existing username
-        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (request.form.get("username"),)
+        )
         rows = cursor.fetchall()
 
         if len(rows) == 1:
             return render_template("register.html")
-
+        
         # Insert username and password
-        cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (request.form.get(
-            "username"), generate_password_hash(password)))
 
-        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        # user=User(request.form.get("username"), generate_password_hash(request.form.get("password")))
+        # db.session.add(user)
+        # db.session.commit()
+
+        cursor.execute(
+            "INSERT INTO users (username, hash) VALUES(?, ?)",
+            (request.form.get("username"), generate_password_hash(password)),
+        )
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (request.form.get("username"),)
+        )
         rows = cursor.fetchall()
+
         session["user_id"] = rows[0][0]
-
         table.commit()
-        return redirect("/")
 
+        return redirect("/")
+    
     else:
         return render_template("register.html")
 
-    
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 @app.route("/artifact", methods=["GET", "POST"])
 def artifact():
-
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
 
@@ -198,7 +296,6 @@ def artifact():
 
 @app.route("/commit", methods=["POST", "GET"])
 def commit():
-
     table = sqlite3.connect("data/database.db")
     cursor = table.cursor()
 
@@ -291,5 +388,5 @@ def commit():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 1234))
+    port = int(os.environ.get("PORT", 1000))
     app.run(debug=True, host="0.0.0.0", port=port)
